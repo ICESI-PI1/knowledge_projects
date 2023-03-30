9078026d2f7c784436597408f2567ced4749c816
unknown <1110282139@u.icesi.edu.co>
2023-03-29 23:30:11 -0500

    Add data model and migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client,Project,Employee,Rol
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(Employee)
+admin.site.register(Rol)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    #Primary keiy
+    nit = models.CharField(primary_key=True,max_length=11)
+
+    name = models.CharField(max_length=30)
+    phoneNumber = models.CharField(max_length=20)
+    adress = models.CharField(max_length=10)
+    representativeName = models.TextField()
+    phoneNumberRepresentative = models.CharField(max_length=10)
+    
+
+class Project(models.Model):
+    #Primary key
+    projectId = models.CharField(primary_key=True,max_length=5)
+    #Foreing Key
+    nit = models.ForeignKey(Client,on_delete = models.CASCADE)
+
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    result = models.TextField()
+    scope = models.TextField()
+    workPlan = models.TextField()
+    budget = models.DecimalField(max_digits=10,decimal_places=0)
+    humanCapital = models.TextField()
+    state = models.CharField(max_length=20)
+    goal = models.TextField()
+    asssignedResources = models.TextField()
+    category = models.CharField(max_length=20)
+    convocatoryNumber = models.IntegerField()
+
+  
+    
+class Convocatory(models.Model):
+    #Primary key
+    convocatoryId = models.CharField(max_length=5)
+    #Foreign key
+    projectId = models.ForeignKey(Project,models.CASCADE)
+    startDate = models.DateField()
+    closingDate = models.DateField()
+
+class Binnacle(models.Model):
+    #Primary key
+    binnacleId = models.CharField(max_length=5)
+    #Foreing key
+    projectId = models.ForeignKey(Project,on_delete = models.CASCADE)
+
+class Log(models.Model):
+
+    #Primary key
+    logId = models.CharField(max_length=10)
+    #Foreing Key
+    binnacleId = models.ForeignKey(Binnacle,models.CASCADE)
+    
+    dateTime = models.DateTimeField()
+    observations = models.TextField()
+    results = models.TextField()
+
+class Rol(models.Model):
+    name = models.CharField(primary_key=True,max_length=20)
+
+class Employee(models.Model):
+    #Primary key
+    id = models.CharField(primary_key=True,max_length=10)
+
+    #Foreign key
+    role  = models.ForeignKey(Rol,on_delete = models.CASCADE)
+
+
+    fullName = models.TextField()
+    picture = models.ImageField()
+    birthDate = models.DateField()
