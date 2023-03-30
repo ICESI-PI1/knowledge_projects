21876c8bc78514219a76f2def15024e1d7e57b9f
tyronemab <jhonatancastanoibarra@gmail.com>
2023-03-30 00:19:54 -0500

    Add auth

---
+++ Auth/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+# Create your views here.
+
+def register(request):
+    if request.method == 'GET':
+        form = UserCreationForm(request.GET)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    context = {'form':form}
+    return render(request,'Auth/register.html',context)
+
+def home(request):
+    return render(request,'Auth/home.html')
