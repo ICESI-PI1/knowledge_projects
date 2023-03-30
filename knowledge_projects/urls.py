f89de577c7100f7e347d16fe63f64857e7b1b853
Samhernesp <93722444+Samhernesp@users.noreply.github.com>
2023-03-29 23:30:03 -0600

    Update urls.py

--- knowledge_projects/urls.py
+++ knowledge_projects/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Auth.urls'))
+    path('',include('core.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
