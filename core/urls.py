2edc895319fa246ec8a40d2497d303daf92ac3b1
davidps79 <1006352045@u.icesi.edu.co>
2023-05-07 17:46:45 -0500

    Add main view and connect with auth

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
-
 from django.urls import include, path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from core import views
 
 app_name = 'core'
 
 urlpatterns = [
-    path('home/', views.home, name ='home'),
+    path('', views.Home_view.as_view(), name='home'),
+    path('home/', views.Home_view.as_view(), name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views import View
 from Auth.decorators import client_required,employee_required
 
 
@@ -8,8 +9,13 @@ from .models import Card
 
 # Create your views here.
 
-@login_required
-@client_required
-def home(request):
-    obj = Card.objects.all()
-    return render(request,'index.html',{'card':obj})
+class Home_view(View):   
+    def get(self, request):
+        
+        username = None
+        if request.user.is_authenticated:
+            username = request.user.username
+
+        context = {"username" : 'Hola, ' + username.capitalize(),}
+
+        return HttpResponse(render(request,'home_view.html',context))
