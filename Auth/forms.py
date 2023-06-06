f0e1ffd484aae60306906e33f6aed2b7ba748640
davidps79 <1006352045@u.icesi.edu.co>
2023-06-06 01:05:38 -0500

    Fix auth ui

--- Auth/forms.py
+++ Auth/forms.py
@@ -5,24 +5,42 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class User_login_form(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
     class Meta :
         model = User
         fields = [
-            'username'
-            'password'
         ]
         widgets = {
-            'username' : forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),required=True),
-            'password': forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'type':'password','placeholder': ''}),required=True),
+        }
+
+class Client_register_form(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': ' '}),required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
+    class Meta:
+        model = Client
+        
+        fields = [
+            'name',
+            'phone_number',
+            'address',
+            'representative_name',
+            'phone_number_representative'
+        ]
+        widgets = { 
+            'name':  forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+            'phone_number':forms.NumberInput(attrs={'class': 'input', 'placeholder': ''}),
+            'address':forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+            'representative_name':forms.TextInput(attrs={'class': 'input', 'placeholder': ' '}),
+            'phone_number_representative':forms.NumberInput(attrs={'class': 'input', 'placeholder': ''}),
         }
 
 
 class Employee_register_form(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Contraseña'}))
-
     password_confirmation = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Confirmar contraseña'}))
-
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Cedula'}))
 
     class Meta:
@@ -86,31 +104,6 @@ class Employee_edit_form(forms.ModelForm):
             'birth_date': forms.DateInput(attrs={'type': 'date','class': 'form-control', 'placeholder': 'Fecha de nacimiento:'}),
             'img':forms.FileInput(attrs={'class': 'form-control mb-1', 'placeholder': 'Imagen:'}),
         
-        }
-
-
-class Client_register_form(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
-
-    password_confirmation = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder': ' '}),required=True)
-
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),required=True)
-    class Meta:
-        model = Client
-        
-        fields = [
-            'name',
-            'phone_number',
-            'address',
-            'representative_name',
-            'phone_number_representative'
-        ]
-        widgets = { 
-            'name':  forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
-            'phone_number':forms.NumberInput(attrs={'class': 'input', 'placeholder': ''}),
-            'address':forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
-            'representative_name':forms.TextInput(attrs={'class': 'input', 'placeholder': ' '}),
-            'phone_number_representative':forms.NumberInput(attrs={'class': 'input', 'placeholder': ''}),
         }
 
         
